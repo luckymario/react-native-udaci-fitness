452854a893cec500c518b7109fb32966c0daa8b5
Mario <kowalewski.mariusz@gmail.com>
2018-10-12 19:18:10 +0200

    style: Add styles to components

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, Platform, StyleSheet } from 'react-native'
 import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
@@ -10,12 +10,14 @@ import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { getDailyReminderValue } from '../utils/helpers'
+import { white, purple } from '../utils/colors'
 
 function SubmitBtn ({ onPress }) {
 	return (
 		<TouchableOpacity
+			style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
 			onPress={onPress}>
-				<Text>SUBMIT</Text>
+				<Text style={styles.sumbitBtnText}>SUBMIT</Text>
 		</TouchableOpacity>
 	)
 }
@@ -101,13 +103,13 @@ class AddEntry extends Component {
 
 		if (this.props.alreadyLogged) {
 			return (
-				<View>
+				<View style={styles.center}>
 					<Ionicons
-						name='ios-happy-outline'
+						name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
 						size={100}
 					/>
 					<Text>You already logged your information today</Text>
-					<TextButton onPress={this.reset}>
+					<TextButton style={{padding: 10}} onPress={this.reset}>
 						Reset
 					</TextButton>
 				</View>
@@ -115,14 +117,14 @@ class AddEntry extends Component {
 		}
 
 		return (
-			<View>
+			<View style={styles.container}>
 				<DateHeader date={(new Date()).toLocaleDateString()} />
 				{Object.keys(metaInfo).map((key) => {
 					const { getIcon, type, ...rest } = metaInfo[key]
 					const value = this.state[key]
 
 					return (
-						<View key={key}>
+						<View key={key} style={styles.row}>
 							{getIcon()}
 							{type === 'slider'
 								? <UdaciSlider
@@ -144,6 +146,50 @@ class AddEntry extends Component {
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		backgroundColor: white
+	},
+	row: {
+		flexDirection: 'row',
+		flex: 1,
+		alignItems: 'center'
+	},
+	iosSubmitBtn: {
+		backgroundColor: purple,
+		padding: 10,
+		borderRadius: 7,
+		height: 45,
+		marginLeft: 40,
+		marginRight: 40
+	},
+	androidSubmitBtn: {
+		backgroundColor: purple,
+		padding: 10,
+		borderRadius: 2,
+		height: 45,
+		marginLeft: 30,
+		marginRight: 30,
+		alignSelf: 'flex-end',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	sumbitBtnText: {
+		color: white,
+		fontSize: 22,
+		textAlign: 'center'
+	},
+	center: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 30,
+		marginRight: 30
+	}
+})
 
 function mapStateToProps (state) {
 	const key = timeToString()
