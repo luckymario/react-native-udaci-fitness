9c5e96dbd886e368b0233223ebbe2959dfaa2eb5
Mario <kowalewski.mariusz@gmail.com>
2018-10-15 21:22:46 +0200

    feat: Ask location permissions

--- components/Live.js
+++ components/Live.js
@@ -2,20 +2,63 @@ import React, { Component } from 'react'
 import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
+import { Location, Permissions } from 'expo'
+import { calculateDirection } from '../utils/helpers'
 
 export default class Live extends Component {
 	state = {
 		coords: null,
-		status: 'granted',
+		status: 'undetermined',
 		direction: ''
 	}
 
-	askPermission = () => {
+	componentDidMount () {
+		Permissions.getAsync(Permissions.LOCATION)
+			.then(({ status }) => {
+				if (status === 'granted') {
+					return this.setLocation()
+				}
 
+				this.setState(() => ({ status }))
+			})
+			.catch((error) => {
+				console.warn('Error getting Location permission: ', error)
+
+				this.setState(() => ({ status: 'undetermined' }))
+			})
+	}
+
+	askPermission = () => {
+		Permissions.askAsync(Permissions.LOCATION)
+			.then(({ status }) => {
+				if (status === 'granted') {
+					return this.setLocation()
+				}
+
+				this.setState(() => ({ status }))
+			})
+			.catch((error) => console.warn('Error asking Location permission: ', error))
+	}
+
+	setLocation = () => {
+		Location.watchPositionAsync({
+			enableHighAccuracy: true,
+			timeInterval: 1,
+			distanceInterval: 1
+		}, ({ coords }) => {
+			const newDirection = calculateDirection(coords.heading)
+			const { direction } = this.state
+
+			this.setState(() => ({
+				coords,
+				status: 'granted',
+				direction: newDirection
+			}))
+		})
 	}
 
 	render() {
-		const { status, coord, direction } = this.state
+		const { status, coords, direction } = this.state
 
 		if (status === null) {
 			return <ActivityIndicator style={{marginTop: 30}} />
@@ -37,7 +80,7 @@ export default class Live extends Component {
 				<View style={styles.center}>
 					<Foundation name='alert' size={50} />
 					<Text>
-						You need to enalb location ervices for this app.
+						You need to enable location services for this app.
 					</Text>
 					<TouchableOpacity onPress={this.askPermission} style={styles.button}>
 						<Text style={styles.buttonText}>
@@ -52,7 +95,7 @@ export default class Live extends Component {
 			<View style={styles.container}>
 				<View style={styles.directionContainer}>
 					<Text style={styles.header}>You're heading</Text>
-					<Text style={styles.direction}>North</Text>
+					<Text style={styles.direction}>{direction}</Text>
 				</View>
 				<View style={styles.metricContainer}>
 					<View style={styles.metric}>
@@ -60,7 +103,7 @@ export default class Live extends Component {
 							Altitude
 						</Text>
 						<Text style={[styles.subHeader, { color: white }]}>
-							{200} Feet
+							{Math.round(coords.altitude * 3.2808)} Feet
 						</Text>
 					</View>
 					<View style={styles.metric}>
@@ -68,7 +111,7 @@ export default class Live extends Component {
 							Speed
 						</Text>
 						<Text style={[styles.subHeader, { color: white }]}>
-							{300} MPH
+							{(coords.speed * 2.2369).toFixed(1)} MPH
 						</Text>
 					</View>
 				</View>
